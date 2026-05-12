2e9c90393276e34ba0198eda15fd8e62e6d2e12e
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-05-13 02:49:22 +0600

    firt commit for chat application

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import dotenv from "dotenv"
+const app=express()
+dotenv.config()
+const PORT=process.env.PORT
+
+app.use(express.json())
+app.get("/",(req,res)=>{
+  res.send("api is working")
+})
+app.listen(PORT,()=>{
+  console.log(`server is running on http://localhost:${PORT}`)
+})
